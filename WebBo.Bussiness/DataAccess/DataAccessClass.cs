6821139671533e1876bf5fb2f9f56bdbc9b5c5d8
xieyuliang <xieyuliang1985@126.com>
2018-08-28 18:19:19 +0800

    [mod]临时提交

---
+++ WebBo.Bussiness/DataAccess/DataAccessClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace WebBo.Bussiness.DataAccess
+{
+    public class DataAccessClass
+    {
+        protected string _connectionString;
+
+        internal DataAccessClass(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+        protected IDbDataParameter[] BuildParameters(Hashtable htParams)
+        {
+            var resultList = new List<MySqlParameter>();
+            foreach (DictionaryEntry de in htParams)
+            {
+                resultList.Add(new MySqlParameter(de.Key.ToString(), de.Value));
+            }
+            return resultList.ToArray();
+        }
+
+
+
+
+        protected DataTable GetDataTable(string sql, IDbDataParameter[] parameters)
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection con = new MySqlConnection(_connectionString))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(sql, con))
+                {
+                    //添加参数
+                    adapter.SelectCommand.Parameters.AddRange(parameters);
+                    //1.打开链接，如果连接没有打开，则它给你打开；如果打开，就算了
+                    //2.去执行sql语句，读取数据库
+                    //3.sqlDataReader,把读取到的数据填充到内存表中
+                    adapter.Fill(dt);
+                }
+            }
+            return dt;
+        }
+
+
+        /// <summary>
+        /// 执行增删改，返回受影响的行数
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        protected int ExecuteNonQuery(string sql, params IDbDataParameter[] param)
+        {
+            int n = -1;
+            using (MySqlConnection con = new MySqlConnection(_connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                {
+                    cmd.Parameters.AddRange(param);
+                    con.Open();
+                    n = cmd.ExecuteNonQuery();
+                }
+            }
+            return n;
+        }
+
+
+    }
+}
---
+++ WebBo.Bussiness/DataAccess/FctUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebBo.Bussiness.DataAccess
+{
+    public class FctUser: DataAccessClass
+    {
+        public FctUser(string connectionString) : base(connectionString) { }
+
+
+
+        public void GetUser(int userId)
+        {
+            var parameters = base.BuildParameters(new Hashtable
+            {
+                {"userId",userId }
+            });
+            var sql = @"SELECT * FROM weibo.fct_user where id = ?userId";
+            var dtUser = base.GetDataTable(sql, parameters);
+            
+
+
+        }
+    }
+}
